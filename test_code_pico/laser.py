219856c8d0a4d5db2631fd4cbac936f43e18c98f
JesseUijtdeHaag <jesse.uytdehaag@gmail.com>
2023-11-14 14:19:24 +0100

    update cade laser

--- test_code_pico/laser.py
+++ test_code_pico/laser.py
@@ -2,8 +2,11 @@ from machine import Pin
 from time import sleep
 
 laser1 = Pin(21, Pin.IN)
+led = machine.Pin('LED', machine.Pin.OUT)
 
 while(True):
-    print(laser1.value())
-   # if(laser1.value()):
-     #   print("Detected")
+   if(laser1.value()):
+        led.value(True)
+   else:
+       led.value(False)
+
