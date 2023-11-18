f3174cc5ffca1a9bbed3b0818709a2e748942f75
Warre-Willie <warre.willeme@gmail.com>
2023-11-18 15:59:55 +0100

    MySQL database test code

---
+++ test_database/MySQL connection demo/MySQL connection demo/Program.cs
@@ -0,0 +1,36 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySQL_connection_demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string server = "localhost";
+            string database = "demo school";
+            string username = "root";
+            string password = "";
+            string constring = "SERVER=" + server + ";DATABASE=" + database + ";UID=" + username + ";PASSWORD=" + password + ";";
+
+            MySqlConnection conn = new MySqlConnection(constring);
+            conn.Open();
+
+            string query = "SELECT * FROM persoongegevens";
+
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            MySqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine(reader["voornaam"]);
+                Console.WriteLine(reader["achternaam"]);
+                Console.WriteLine("------------------");
+            }
+        }
+    }
+}
