9b7e57e2433a50e280d77ef04fc72b76cbae9bf0
Warre-Willie <warre.willeme@gmail.com>
2023-11-22 15:47:06 +0100

    Update read_uart.py

--- test_code_pico/read_uart.py
+++ test_code_pico/read_uart.py
@@ -1,12 +1,12 @@
 from machine import Pin,UART
 import time
 uart = UART(1, baudrate=9600, tx=Pin(4), rx=Pin(5))
-uart.init(bits=8, parity=None, stop=2)
-led = Pin("LED", Pin.OUT)
+uart.init(bits=8, parity=None, stop=1)
 
 while True:
     if uart.any(): 
-        data = uart.read() 
+        data = uart.read()
         if data != "":
+            data = data.decode()
             print(data)
     time.sleep(1)
