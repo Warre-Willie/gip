7300d84aa2dbdb130237c6de215275b30f497a77
JesseUijtdeHaag <jesse.uytdehaag@gmail.com>
2023-11-25 20:31:08 +0100

    begin count handler

---
+++ python_scripts/zone_count_handler.py
@@ -0,0 +1,52 @@
+import network
+import time
+from machine import Pin
+from umqtt.simple import MQTTClient
+
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect("TP-LINK_EE42","29487868")
+time.sleep(5)
+print(wlan.ifconfig())
+
+LED = Pin(0, Pin.OUT)
+
+mqtt_server = '192.168.0.101'
+client_id = 'test2'
+topic_sub = b'led'
+counter = 0
+
+
+def sub_cb(topic, msg):
+    print("New message on topic {}".format(topic.decode('utf-8')))
+    msg = msg.decode('utf-8')
+    
+    if msg != old_msg
+        match msg:
+            case "-":
+                counter - 1
+            case "+":
+                counter + 1
+    
+    msg = old_msg
+def mqtt_connect():
+    client = MQTTClient(client_id, mqtt_server, keepalive=60)
+    client.set_callback(sub_cb)
+    client.connect()
+    print('Connected to %s MQTT Broker'%(mqtt_server))
+    return client
+
+def reconnect():
+    print('Failed to connect to MQTT Broker. Reconnecting...')
+    time.sleep(5)
+    machine.reset()
+    
+try:
+    client = mqtt_connect()
+except OSError as e:
+    reconnect()
+
+
+while True:
+    client.subscribe(topic_sub)
+    time.sleep(1)
