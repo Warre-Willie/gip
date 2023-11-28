87677f7adc4329939ee8d2327576a2031933da2b
Warre <warre.willeme@gmail.com>
2023-11-28 18:44:32 +0100

    mysql api

---
+++ python_scripts/test.py
@@ -0,0 +1,24 @@
+import paho.mqtt.client as mqtt
+
+# Define callback functions
+def on_connect(client, userdata, flags, rc):
+    print(f"Connected with result code {rc}")
+    # Subscribe to the topic when connected
+    client.subscribe("Count_update")
+    client.subscribe("jesse")
+
+def on_message(client, userdata, msg):
+    print(f"Received message on topic {msg.topic}: {msg.payload.decode()}")
+
+# Create an MQTT client instance
+client = mqtt.Client()
+
+# Set callback functions
+client.on_connect = on_connect
+client.on_message = on_message
+
+# Connect to the MQTT broker (replace 'broker_address' and 'port' with your broker's address and port)
+client.connect("broker.hivemq.com", port=1883, keepalive=60)
+
+# Start the MQTT loop to listen for messages
+client.loop_forever()
--- test_code_pico/read_uart.py
+++ test_code_pico/read_uart.py
@@ -1,6 +1,6 @@
 from machine import Pin,UART
 import time
-uart = UART(1, baudrate=9600, tx=Pin(4), rx=Pin(5))
+uart = UART(0, baudrate=9600, tx=Pin(0), rx=Pin(1))
 uart.init(bits=8, parity=None, stop=1)
 
 while True:
