e042af78fe04538af32e76f9786a490676e6fec5
Warre-Willie <warre.willeme@gmail.com>
2023-12-07 22:58:31 +0100

    website added

---
+++ crowd_management/crowd_management/js/settings_panel.js
@@ -0,0 +1,7 @@
+function toggleSettings() {
+  var infoPanel = document.getElementById("divInfoPanel");
+  var settingsPanel = document.getElementById("divSettingsPanel");
+
+  infoPanel.classList.toggle("hide");
+  settingsPanel.classList.toggle("hide");
+}
