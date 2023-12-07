23f59e54fb2e4eecd83a361f7b86fc4657251e78
Warre-Willie <warre.willeme@gmail.com>
2023-12-07 22:58:09 +0100

    website deleted

--- crowd_management/crowd_management/crowd_management/js/settings_panel.js
+++
@@ -1,7 +0,0 @@
-function toggleSettings() {
-  var infoPanel = document.getElementById("divInfoPanel");
-  var settingsPanel = document.getElementById("divSettingsPanel");
-
-  infoPanel.classList.toggle("hide");
-  settingsPanel.classList.toggle("hide");
-}
