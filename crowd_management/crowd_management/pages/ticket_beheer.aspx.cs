15eaa4c4c9e06c86e369ff01408e59582fe34dce
JesseUijtdeHaag <jesse.uytdehaag@gmail.com>
2024-01-05 23:34:15 +0100

    Ticket loading update, progressbar loading done

--- crowd_management/crowd_management/pages/ticket_beheer.aspx.cs
+++ crowd_management/crowd_management/pages/ticket_beheer.aspx.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -14,21 +15,47 @@ namespace crowd_management.pages
         const string constring = "SERVER=localhost;DATABASE=crowd_management;UID=root;PASSWORD=gip-WJ;";
         MySqlConnection conn = new MySqlConnection(constring);
 
+        protected void test(object sender, EventArgs e)
+        {
+            LinkButton linkButton = (LinkButton)sender;
+            Debug.WriteLine(linkButton.ID);
+        }
+
+
         protected void Page_Load(object sender, EventArgs e)
         {
             try
             {
                 //Load the barcode search
-                string query = $"SELECT barcode FROM tickets;";
+                string query = $"SELECT barcode, RFID FROM tickets;";
                 conn.Open();
                 MySqlCommand cmd = new MySqlCommand(query, conn);
                 MySqlDataReader reader = cmd.ExecuteReader();
+
+                int ticketCounter = 0, RFIDCounter = 0;
                 while (reader.Read())
                 {
-                    divTicketList.InnerHtml += $"<asp:LinkButton ID='LinkButton1' runat='server' class='panel-block is-active' OnClientClick='' data-barcode={reader["barcode"]}><span class='panel-icon'><class='fa-solid fa-ticket'></></span>{reader["barcode"]}</asp:LinkButton>";
+                    ticketCounter++;
+
+                    if (!string.IsNullOrEmpty(reader["RFID"].ToString()))
+                    {
+                        RFIDCounter++;
+                    }
+                
+                    LinkButton ticket = new LinkButton();
+                    ticket.Text = reader["barcode"].ToString();
+                    ticket.ID = reader["barcode"].ToString();
+                    ticket.CssClass = "panel-block is-active";
+                    ticket.Click += test;
+                    divTicketList.Controls.Add(ticket);
+                    //divTicketList.InnerHtml += $"<asp:LinkButton ID='LinkButton1' runat='server' class='panel-block is-active' OnClientClick='test' data-barcode={reader["barcode"]}><span class='panel-icon'><class='fa-solid fa-ticket'></></span>{reader["barcode"]}</asp:LinkButton>";
                 }
                 conn.Close();
 
+                progress.Attributes["value"] = RFIDCounter.ToString();
+                progress.Attributes["max"] = ticketCounter.ToString();
+
+                progressValue.InnerHtml = $" {RFIDCounter} / {ticketCounter}";
             }
             catch (Exception ex)
             {
@@ -39,18 +66,15 @@ namespace crowd_management.pages
             try
             {
                 string query = "";
-                conn.Open();
-                MySqlCommand cmd = new MySqlCommand(query, conn);
-                MySqlDataReader reader = cmd.ExecuteReader();
+            conn.Open();
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            MySqlDataReader reader = cmd.ExecuteReader();
 
-                while(reader.Read())
-                {
-                    progress.Attributes["value"] = progress.Attributes["value"].Replace("1",reader["value"].ToString());
-                    progress.Attributes["max"] = progress.Attributes["max"].Replace("10", reader["max"].ToString());
-
-                    progressValue.InnerHtml = $"{reader["value"]} / {reader["max"]}";
-                }
-                conn.Close();
+            while (reader.Read())
+            {
+                
+            }
+            conn.Close();
             }
             catch(Exception ex1)
             {
