3b9464eb9283d263697ff65acac77f2bc9d694e2
JesseUijtdeHaag <jesse.uytdehaag@gmail.com>
2024-01-19 20:38:17 +0100

    Refactor zone count handler and add database update functionality

--- python_scripts/zone_count_handler2.py
+++ python_scripts/zone_count_handler2.py
@@ -12,8 +12,6 @@ import json
 # Global varible
 thresholds = {}
 
-
-
 # Make connection with database
 db = mysql.connector.connect(
     host="localhost",
@@ -21,18 +19,56 @@ db = mysql.connector.connect(
     passwd="gip-WJ",
     database="crowd_management"
     )
-mycursor = db.cursor(dictionary=True) # Dictionary true for ease of processing respones
-mycursor.execute("SELECT * FROM `zones`")
 
-# Set thresholds
-for row in mycursor:
-    if row["people_count"] != None:
-        threshold = {"green": row["threshold_green"], "orange": row["threshold_orange"], "red": row["threshold_red"]}
-        thresholds[row["id"]] = (threshold)
+def database_update():
+    mycursor = db.cursor(dictionary=True) # Dictionary true for ease of processing respones
+    mycursor.execute("SELECT * FROM `zones`")
 
-mycursor.close()
-#print(thresholds[1]['green'])
+    # Set thresholds
+    for row in mycursor:
+        if row["people_count"] != None:
+            threshold = {"green": row["threshold_green"], "orange": row["threshold_orange"], "red": row["threshold_red"]}
+            thresholds[row["id"]] = (threshold)
 
+
+def count_request(msg):
+    counter = 0
+    response = json.loads(msg.payload.decode())
+
+    mycursor = db.cursor(dictionary=True) # Dictionary true for ease of processing respones
+    mycursor.execute(f"SELECT `people_count`, `barometer_lock` FROM `zones` WHERE `id` = '{response['id']}'")
+    for row in mycursor:
+        if row["people_count"] != None:
+            counter = int(row["people_count"])
+            counter += response["people"]
+            if counter < 0:
+                counter = 0
+                mycursor.execute(f"UPDATE `zones` SET `people_count`= '{str(counter)}' WHERE `id` = '{response['id']}'")
+                db.commit()
+                print(counter)
+            else:
+                mycursor.execute(f"UPDATE `zones` SET `people_count`= '{str(counter)}' WHERE `id` = '{response['id']}'")
+                db.commit()
+                print(counter)
+
+            if row["barometer_lock"] == 0:
+                if counter <= thresholds[response['id']]['green']:
+                    client.publish("barometer", '{"id": ' + str(response['id']) + ', "color": "green"}')
+                    mycursor.execute(f"UPDATE `zones` SET `barometer_color`= 'green' WHERE `id` = '{response['id']}'")
+                    db.commit()
+                elif counter <= thresholds[response['id']]['orange']:
+                    client.publish("barometer", '{"id": ' + str(response['id']) + ', "color": "orange"}')
+                    mycursor.execute(f"UPDATE `zones` SET `barometer_color`= 'orange' WHERE `id` = '{response['id']}'")
+                    db.commit()
+                elif counter >= thresholds[response['id']]['red']:
+                    client.publish("barometer", '{"id": ' + str(response['id']) + ', "color": "red"}')
+                    mycursor.execute(f"UPDATE `zones` SET `barometer_color`= 'red' WHERE `id` = '{response['id']}'")
+                    db.commit()
+            else:
+                print("Barometer locked")
+        else:
+            print("No count zone")
+    mycursor.close()
 
 
 # MQTT settings
@@ -43,36 +79,15 @@ port = 1883
 # Callback when a message is received from the broker
 # The incomming data will be in the format of json: {"id": 1,"people": -1}
 def on_message(client, userdata, msg):
-    counter = 0
-    response = json.loads(msg.payload.decode())
-
-    mycursor = db.cursor(dictionary=True) # Dictionary true for ease of processing respones
-    mycursor.execute(f"SELECT `people_count`, `barometer_lock` FROM `zones` WHERE `id` = '{response['id']}'")
-    for row in mycursor:
-        if row["people_count"] != None or row["people_count"] != 0:
-            counter = int(row["people_count"])
-            counter += response["people"]
-            mycursor.execute(f"UPDATE `zones` SET `people_count`= '{str(counter)}' WHERE `id` = '{response['id']}'")
-            db.commit()
-            print(counter)
-        else:
-            print("No data")
-
-        if row["barometer_lock"] == 0 and row["people_count"] != None:
-            if counter <= thresholds[response['id']]['green']:
-                client.publish(f"barometer", '{"id": ' + str(response['id']) + ', "color": "green"}')
-                mycursor.execute(f"UPDATE `zones` SET `barometer_color`= 'green' WHERE `id` = '{response['id']}'")
-                db.commit()
-            elif counter <= thresholds[response['id']]['orange']:
-                client.publish(f"barometer", '{"id": ' + str(response['id']) + ', "color": "orange"}')
-                mycursor.execute(f"UPDATE `zones` SET `barometer_color`= 'orange' WHERE `id` = '{response['id']}'")
-                db.commit()
-            elif counter >= thresholds[response['id']]['red']:
-                client.publish(f"barometer", '{"id": ' + str(response['id']) + ', "color": "red"}')
-                mycursor.execute(f"UPDATE `zones` SET `barometer_color`= 'red' WHERE `id` = '{response['id']}'")
-                db.commit()
-        else:
-            print("Barometer locked")
+    match = msg.topic
+    if match == "Jesse":
+        count_request(msg)
+    elif match == "db_update":
+        database_update()
+        print("Database updated")
+    else:
+        print("No match")
+        
       
 # Create MQTT client instance with no client_id
 client = mqtt.Client(client_id="", clean_session=True)
@@ -82,10 +97,14 @@ client = mqtt.Client(client_id="", clean_session=True)
 # client.on_connect = on_connect
 client.on_message = on_message
 
+#First database update to get the thresholds
+database_update()
+
 # Connect to the broker
 client.connect(broker_address, port, 60)
 
 client.subscribe("Jesse")
+client.subscribe("db_update")
 # Start the network loop
 while True:
     client.loop_start()
